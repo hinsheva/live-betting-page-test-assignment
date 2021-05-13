38acb78aa348a53e9ff2814658fa5ef73c3a4883
Elena Insheva <elena.insheva@kindredgroup.com>
2021-05-13 21:25:14 +0200

    app configs

---
+++ .bablerc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/env',
+        {
+          targets: '> 0.25%, not dead, safari 11',
+        },
+      ],
+      '@babel/react',
+      '@babel/preset-flow',
+    ],
+    plugins: ['@babel/plugin-proposal-export-default-from'],
+  };
